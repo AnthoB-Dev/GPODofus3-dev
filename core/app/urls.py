b5fff3a71ee8b46fa280f258036f4c3f2dc0a01a
Anthony <bonis.anthony@gmail.com>
2024-11-04 01:18:10 +0100

    shredded core views and urls

--- core/app/urls.py
+++ core/app/urls.py
@@ -1,12 +1,7 @@
 from django.urls import path
 from . import views
+# from .views import CharacterListView
 
 urlpatterns = [
-    path('', views.App, name='index'),
-    path('groupes', views.Group, name='groups'),
-    path('personnages', views.CharacterViewSet.Character, name='characters'),
-    path('succes', views.Achievement, name='achievements'),
-    path('quetes', views.Quest, name='quests'),
-    # path('menu', views.Menu, name='menu'),
-    # path('zone', views.Zone, name='zone'),
+    path("", views.DebugView, name="debug_view"),
 ]

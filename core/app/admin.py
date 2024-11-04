cefb6ac498e5720e36b6c957c48c612219297447
Anthony <bonis.anthony@gmail.com>
2024-11-04 01:18:55 +0100

    and more

--- core/app/admin.py
+++ core/app/admin.py
@@ -1,23 +1,42 @@
 from django.contrib import admin
 
-from .models import Character, Group
+from .models import AchievementQuest, Alignment, GuideAchievement, Quest, Achievement, Guide, CommonSpell
 
-# Register your models here.
-class CharacterAdmin(admin.ModelAdmin):
-    list_display = ('name', 'group')
-    list_select_related = ("group", )
+
+class AchievementAdmin(admin.ModelAdmin):
+    list_display = ('title', 'completion_points', 'created_in_version', 'updated_for_version', 'updated_at')
+    list_filter = ('completion_points', 'created_in_version', 'updated_at')
+    search_fields = ('title',)
+
+class GuideAchievementInline(admin.TabularInline):
+    model = GuideAchievement
+    extra = 1  
+
+class GuideAdmin(admin.ModelAdmin):
+    inlines = [GuideAchievementInline]  
+    list_display = ['title']
     
-    def get_group_name(self, obj):
-        return obj.group.name  # Remplacez "name" par le champ souhaité du modèle Group
+class AchievementQuestInline(admin.TabularInline):
+    model = AchievementQuest
+    extra = 1  
 
-class CharacterInline(admin.TabularInline):  # Utilisez `StackedInline` si vous préférez un affichage en blocs
-    model = Character
-    extra = 1  # Affiche un champ vide pour ajouter un nouveau personnage directement dans l'admin
-    fields = ('name', 'group')  # Spécifie les champs de Character que vous souhaitez afficher dans l'inline
+class AchievementAdmin(admin.ModelAdmin):
+    inlines = [AchievementQuestInline]
+    list_display = ['title', 'id', 'created_in_version']
 
-class GroupAdmin(admin.ModelAdmin):
-    inlines = [CharacterInline]  # Ajout de l'inline dans GroupAdmin pour afficher les personnages associés
+class QuestAchievementInline(admin.TabularInline):
+    model = AchievementQuest
+    extra = 1  
 
+class QuestAdmin(admin.ModelAdmin):
+    inlines = [QuestAchievementInline]  
+    list_display = ['title']
+    
+class AlignmentAdmin(admin.ModelAdmin):
+    list_display = ['name', 'id']
 
-admin.site.register(Character, CharacterAdmin)
-admin.site.register(Group, GroupAdmin)
+admin.site.register(Alignment, AlignmentAdmin)
+admin.site.register(Quest, QuestAdmin)
+admin.site.register(Achievement, AchievementAdmin)
+admin.site.register(Guide, GuideAdmin)
+admin.site.register(CommonSpell)

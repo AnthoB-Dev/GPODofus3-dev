db757fb5c2dc2625b2979a96b13a1f41fe02dc06
Anthony <bonis.anthony@gmail.com>
2024-11-08 17:40:11 +0100

    Update admin.py

--- core/app/admin.py
+++ core/app/admin.py
@@ -1,39 +1,65 @@
 from django.contrib import admin
 
-from .models import AchievementQuest, Alignment, GuideAchievement, Quest, Achievement, Guide, CommonSpell
+from .models import (
+    AchievementQuest,
+    Alignment,
+    GuideAchievement,
+    Quest,
+    Achievement,
+    Guide,
+    CommonSpell,
+)
 
-
-class AchievementAdmin(admin.ModelAdmin):
-    list_display = ('title', 'completion_points', 'created_in_version', 'updated_for_version', 'updated_at')
-    list_filter = ('completion_points', 'created_in_version', 'updated_at')
-    search_fields = ('title',)
 
 class GuideAchievementInline(admin.TabularInline):
     model = GuideAchievement
-    extra = 1  
+    extra = 1
+
 
 class GuideAdmin(admin.ModelAdmin):
-    inlines = [GuideAchievementInline]  
-    list_display = ['title']
-    
-class AchievementQuestInline(admin.TabularInline):
-    model = AchievementQuest
-    extra = 1  
+    inlines = [GuideAchievementInline]
+    list_display = [
+        "title",
+        "id",
+    ]
+    search_fields = ("title",)
+    inlines = [GuideAchievementInline]
 
-class AchievementAdmin(admin.ModelAdmin):
-    inlines = [AchievementQuestInline]
-    list_display = ['title', 'id', 'created_in_version']
 
 class QuestAchievementInline(admin.TabularInline):
     model = AchievementQuest
-    extra = 1  
+    extra = 1
+
 
 class QuestAdmin(admin.ModelAdmin):
-    inlines = [QuestAchievementInline]  
-    list_display = ['title']
-    
+    inlines = [QuestAchievementInline]
+    list_display = ["title", "id"]
+    search_fields = ("title",)
+    inlines = [QuestAchievementInline]
+
+
+class AchievementQuestInline(admin.TabularInline):
+    model = AchievementQuest
+    extra = 1
+
+
+class AchievementAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "id",
+        "completion_points",
+        "created_in_version",
+        "updated_for_version",
+        "updated_at",
+    )
+    list_filter = ("completion_points", "created_in_version", "updated_at")
+    search_fields = ("title",)
+    inlines = [AchievementQuestInline]
+
+
 class AlignmentAdmin(admin.ModelAdmin):
-    list_display = ['name', 'id']
+    list_display = ["name", "id"]
+
 
 admin.site.register(Alignment, AlignmentAdmin)
 admin.site.register(Quest, QuestAdmin)

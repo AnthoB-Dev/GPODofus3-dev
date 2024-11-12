ed8b3df6820f96b4a20ef2364d706f0ef91b70cd
Anthony <bonis.anthony@gmail.com>
2024-11-12 14:10:44 +0100

    Improvements on quests.js

--- core/staticfiles/js/quests.js
+++ core/staticfiles/js/quests.js
@@ -3,7 +3,7 @@ const toggleQuest = (questId) => {
     method: "POST",
     headers: {
       "X-CSRFToken": document.querySelector("[name=csrfmiddlewaretoken]").value,
-      "Content-Type": "text/html",
+      "Content-Type": "application/json",
     },
   })
     .then((response) => response.text())
@@ -22,10 +22,6 @@ const getLinks = () => {
   return links;
 };
 
-/**
- * Ouvre tous les liens des quêtes d'un succès avec un délais.
- * Délais qui ne permet malheureusement pas de ne pas se faire spot comme popup par le navigateur même avec 5000ms.
- */
 const openLinks = () => {
   const links = getLinks();
   let delay = 0;
@@ -45,78 +41,63 @@ const listenToOpenAll = () => {
   });
 };
 
-/**
- * Appuie sur tous les boutons de toggle de completion des quêtes avec du délais pour éviter des problèmes de chargement.
- */
-const pushValidateAll = (buttonType) => {
+const getButtons = () => {
   const cButtons = document.querySelectorAll(".js-quest .check");
   const uButtons = document.querySelectorAll(".js-quest .uncheck");
-  let buttons = [];
+  let buttonsType = [];
 
-  if (buttonType !== "check" && buttonType !== "uncheck") {
-    console.error("Type de bouton invalide");
-    return;
-  } else {
-    if (buttonType === "check") {
-      buttons = cButtons;
-    } else if (buttonType === "uncheck") {
-      buttons = uButtons;
-    }
+  if (cButtons.length === 0) {
+    buttonsType = uButtons;
+  } else if (cButtons.length > 0) {
+    buttonsType = cButtons;
+  }
+  return buttonsType;
+};
 
-    let delay = 0;
-    buttons.forEach((button) => {
-      setTimeout(() => {
-        button.click();
-      }, delay);
-      delay += 150;
-    });
+const validateAllStyle = () => {
+  const validateAll = document.querySelector("#validateAll");
+  const icon = validateAll.querySelector("i");
+  let buttons = getButtons();
+
+  if (buttons.length > 0 && buttons[0].classList.contains("uncheck")) {
+    const newIcon = document.createElement("i");
+    newIcon.classList.add("fa-solid", "fa-xmark");
+    validateAll.removeChild(icon);
+    validateAll.textContent = "Dévalider tout";
+    validateAll.appendChild(newIcon);
   }
 };
 
-/**
- * Ecoute les évènements sur le bouton de validation de toutes les quêtes.
- */
+const pushValidateAll = (buttonsType) => {
+  let delay = 0;
+  buttonsType.forEach((button) => {
+    setTimeout(() => {
+      button.click();
+    }, delay);
+    delay += 100;
+  });
+};
+
+const toggleBtnBackgroundStyle = () => {
+  const validateAll = document.querySelector("#validateAll");
+  let buttons = getButtons();
+  const color = buttons[0].classList.contains("check") ? "#23DC3D" : "#f37f81";
+
+  validateAll.addEventListener("mouseover", () => {
+    buttons.forEach((button) => {
+      button.style.backgroundColor = color;
+    });
+  });
+  validateAll.addEventListener("mouseout", () => {
+    buttons.forEach((button) => {
+      button.style.backgroundColor = "";
+    });
+  });
+};
+
 const listenToValidateAll = () => {
   const validateAll = document.querySelector("#validateAll");
-  const checkedButtons = document.querySelectorAll(".js-quest .check");
-  const uncheckedButtons = document.querySelectorAll(".js-quest .uncheck");
-  let buttonsType = "check";
-
-  // S'il n'y a aucun bouton de check, cela signifie que toutes les quêtes sont validées.
-  // Préparation pour la dévalidation de toutes les quêtes.
-  if (checkedButtons.length === 0) {
-    // l'event mouseover et mouseout appliquent la couleur #f37f81 sur les boutons à dévalidés.
-    // Les autres boutons restent inchangés.
-    buttonsType = "uncheck";
-
-    validateAll.addEventListener("mouseover", () => {
-      uncheckedButtons.forEach((cButton) => {
-        cButton.style.backgroundColor = "#f37f81";
-      });
-    });
-
-    validateAll.addEventListener("mouseout", () => {
-      uncheckedButtons.forEach((cButton) => {
-        cButton.style.backgroundColor = "";
-      });
-    });
-  } else if (checkedButtons.length >= 0 && uncheckedButtons.length >= 0) {
-    // S'il y a les deux types de boutons, on privilégie la validation des restant.
-    // l'event mouseover et mouseout appliquent la couleur #23DC3D sur les boutons à validés.
-    // Les autres boutons restent inchangés.
-    validateAll.addEventListener("mouseover", () => {
-      checkedButtons.forEach((cButton) => {
-        cButton.style.backgroundColor = "#23DC3D";
-      });
-    });
-
-    validateAll.addEventListener("mouseout", () => {
-      checkedButtons.forEach((cButton) => {
-        cButton.style.backgroundColor = "";
-      });
-    });
-  }
-
+  let buttonsType = getButtons();
   validateAll.addEventListener("click", () => {
     pushValidateAll(buttonsType);
   });
@@ -124,7 +105,9 @@ const listenToValidateAll = () => {
 
 const launchFunctions = () => {
   listenToOpenAll();
+  toggleBtnBackgroundStyle();
   listenToValidateAll();
+  validateAllStyle();
 };
 
 document.addEventListener("DOMContentLoaded", launchFunctions);

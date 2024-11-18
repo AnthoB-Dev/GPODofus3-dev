57b9fe98b3346f7af59143c519dff889f2062f6a
Anthony <bonis.anthony@gmail.com>
2024-11-18 18:48:08 +0100

    Fix of topNav & documentation

--- staticfiles/js/nav.js
+++ staticfiles/js/nav.js
@@ -1,28 +1,55 @@
 /**
- * Met à jour le titre.\ 
- * Puis updateSelectedGuide()
+ * - Met à jour le titre du guide de topNav.
+ * - Puis updateSelectedGuide()
  */
 document.addEventListener("turbo:before-render", (event) => {
-  // Récupérer le nouveau titre depuis la réponse
+  
+  // Récupère le nouveau titre depuis la réponse
   const newTitle = event.detail.newBody.querySelector(
     "#topNav .guide-header h2"
   )?.textContent;
 
   if (newTitle) {
-    // Mettre à jour le titre dans topNav
     const currentTitle = document.querySelector(
       "#topNav .guide-header h2"
     );
     if (currentTitle) {
       currentTitle.textContent = newTitle;
     }
-    // Mettre à jour la sélection dans le dropdown
     updateSelectedGuide();
   }
 });
 
+// Appel topNavHandler() après chaque chargement Turbo.
+// Appel clickFirstAchievementNotCompleted() après 200ms pour laisser le temps à topNavHandler() de faire ce qu'il a à faire. Délais min : 100ms
+document.addEventListener("turbo:load", () => {
+  topNavHandler();
+  setTimeout(() => {
+    clickFirstAchievementNotCompleted();
+  }, 200);
+});
+
 /**
- * Simule un clique sur le premier achievement non complété
+ * - Met à jour la sélection du dropdown.
+ * - Puis clickFirstAchievementNotCompleted()
+ */
+const updateSelectedGuide = () => {
+  const currentTitle = document.querySelector(
+    "#topNav .guide-header h2"
+  )?.textContent;
+  const guideItems = document.querySelectorAll(".guide-item");
+
+  guideItems.forEach((item) => {
+    if (item.textContent.trim() === currentTitle?.trim()) {
+      item.classList.add("selected");
+    } else {
+      item.classList.remove("selected");
+    }
+  });
+};
+
+/**
+ * - Simule un clique sur le premier achievement non complété.
  */
 const clickFirstAchievementNotCompleted = () => {
   const percentages = document.querySelectorAll(".js-achievementPercent");
@@ -40,84 +67,29 @@ const clickFirstAchievementNotCompleted = () => {
 }
 
 /**
- * Met à jour la sélection du dropdown.\
- * Puis clickFirstAchievementNotCompleted()
+ * - Ajoute un état "initialized".
+ * - Ajoute un listener sur la topNav (guide-header) pour ouvrir / fermer le dropdown\
+ * avec toggleDropDown()
+ * - Ajoute un listener sur chaque guide-item pour changer de guide + Remplace .selected pour le mettre sur l'item nouvellement sélectionné\
+ * puis toggleDropdown()
+ * - Ajoute un listener sur le document pour fermer le dropdown lors d'un clique en dehors du dropdown.
  */
-const updateSelectedGuide = () => {
-  const currentTitle = document.querySelector(
-    "#topNav .guide-header h2"
-  )?.textContent;
-  const guideItems = document.querySelectorAll(".guide-item");
-
-  guideItems.forEach((item) => {
-    if (item.textContent.trim() === currentTitle?.trim()) {
-      item.classList.add("selected");
-    } else {
-      item.classList.remove("selected");
-    }
-  });
-
-  clickFirstAchievementNotCompleted();
-};
-
-
-const toggleCaret = () => {
-  const caret = document.querySelector("#topNav i");
-  caret.classList.toggle("caretClose");
-  caret.classList.toggle("caretOpen");
-}
-
-
-/**
- * Ouvre le dropdown
- */
-const openDropdown = () => {
-  const overflow = document.querySelector('.overflow');
-
-  overflow.classList.remove('overflowClose');
-  overflow.classList.remove('hidden');
-  setTimeout(() => {
-    overflow.classList.add('overflowOpen');
-  }, 10);
-}
-
-/**
- * Ferme le dropdown.\
- * Classes CSS:\
- * .overflowOpen\
- * .overflowClose
- */
-const closeDropdown = () => {
-  const overflow = document.querySelector('.overflow');
-  overflow.classList.remove('overflowOpen');
-  setTimeout(() => {
-    overflow.classList.add('overflowClose');
-    overflow.classList.add('hidden');
-  }, 10);
-}
-
-/**
- * Initialisation du dropdown.\
- */
-const initializeDropdown = () => {
+const topNavHandler = () => {
   const topNav = document.getElementById("topNav");
-
   const guideHeader = topNav.querySelector(".guide-header");
-
   const dropDownContent = topNav.querySelector(".dropdown-content");
   const overflow = dropDownContent.querySelector(".overflow");
-
 
   if (!guideHeader.hasAttribute("data-initialized")) {
     guideHeader.setAttribute("data-initialized", "true");
 
-    guideHeader.addEventListener("click", () => {
-      dropDownContent.classList.toggle("hidden");
-      toggleCaret();
-
-      // Scroll vers l'élément sélectionné quand on ouvre le dropdown
-      if (!dropDownContent.classList.contains("hidden")) {
-        openDropdown()
+    // Listener sur le guide-header pour ouvrir / fermer le dropdown
+    guideHeader.addEventListener("click", (event) => {
+      event.stopPropagation(); // Empêche la propagation de l'événement pour éviter de fermer immédiatement
+      if (!topNav.classList.contains("js-open")) {
+        toggleDropdown();
+        
+        // Scroll vers l'élément sélectionné lors de l'ouverture du dropdown
         setTimeout(() => {
           const selectedItem = overflow.querySelector(".guide-item.selected");
           if (selectedItem) {
@@ -126,39 +98,83 @@ const initializeDropdown = () => {
               block: "center",
             });
           }
-        }, 0);
+        }, 10);
       } else {
-        closeDropdown()
+        toggleDropdown();
       }
     });
 
+    // Listener sur chaque guide-item pour changer de guide
     overflow.querySelectorAll(".guide-item").forEach((item) => {
       item.addEventListener("click", (e) => {
         e.preventDefault();
 
-        // Mise à jour de la sélection
+        // Mise à jour de l'item sélectionné .selected
         document
           .querySelectorAll(".guide-item")
           .forEach((i) => i.classList.remove("selected"));
         item.classList.add("selected");
 
+        // Ajout d'un timeout avant le toggle pour permettre la fermeture du dropdown de manière fluide.
+        setTimeout(() => {
+          toggleDropdown();
+        }, 100);
+
         Turbo.visit(item.dataset.url, {
           action: "replace",
           frame: "frame_main",
         });
-        closeDropdown();
-        toggleCaret();
       });
+      
     });
+
+    // Listener sur le document pour fermer le dropdown lors d'un clique en dehors du dropdown
+    document.addEventListener("click", (e) => {
+      if (!topNav.contains(e.target) && !e.target.classList.contains("guide-item") && topNav.classList.contains("js-open")) {
+        toggleDropdown();
+      }
+    })
   }
 };
 
-// Appeler initializeDropdown lors du chargement initial de la page
-document.addEventListener("DOMContentLoaded", () => {
-  initializeDropdown();
-});
+/**
+ * - Toggle l'ouverture / fermeture du dropdown.
+ * - Ajuste le border-radius du topNav selon l'état du dropdown.
+ * - Puis toggleCaret()
+ */
+const toggleDropdown = () => {
+  const topNav = document.getElementById("topNav");
+  const overflow = document.querySelector('.overflow');
+  const dropDownContent = topNav.querySelector(".dropdown-content");
 
-// Appeler initializeDropdown après chaque chargement Turbo
-document.addEventListener("turbo:load", () => {
-  initializeDropdown();
-});
+  if (!topNav.classList.contains("js-open")) {
+    topNav.classList.remove('js-close');
+    topNav.classList.add('js-open');
+    overflow.classList.remove('hidden');
+    dropDownContent.classList.remove('hidden');
+
+    // Force le recalcul du style permettant à la transition de se jouer
+    overflow.offsetHeight;
+    overflow.classList.add('overflowOpen');
+    overflow.classList.remove('overflowClose');
+  } else {
+    topNav.classList.remove('js-open');
+    topNav.classList.add('js-close');
+    overflow.classList.remove('overflowOpen');
+    overflow.classList.add('overflowClose');
+    setTimeout(() => {
+      overflow.classList.add('hidden');
+      dropDownContent.classList.add('hidden');
+    }, 100); // Correspond à la durée de la transition
+  }
+  toggleCaret();
+}
+
+/**
+ * - Toggle l'icône du dropdown.
+ */
+const toggleCaret = () => {
+  const caret = document.querySelector("#topNav i");
+  caret.classList.toggle("caretClose");
+  caret.classList.toggle("caretOpen");
+}

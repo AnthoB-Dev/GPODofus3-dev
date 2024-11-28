fa0cc638e392daaa3b5e452f0df15044f133c93e
Anthony <bonis.anthony@gmail.com>
2024-11-28 01:43:42 +0100

    Add a eventsInitialized flag & listener on turbo:submit-end
    Also moved events on frame-render to frame-load. Not sure yet but it works.

--- static/js/app.js
+++ static/js/app.js
@@ -3,39 +3,79 @@ import * as quests from "./quests.js";
 import * as achievements from "./achievements.js";
 import * as guide from "./guide.js";
 
+let eventsInitialized = false;
+
 // Fonction pour initialiser les événements
 const initializeEvents = () => {
+    // Toujours purger les événements avant de les réinitialiser
+    purgeEvents();
+
+    nav.updateSelectedGuide();
     nav.addNavEventListeners();
     quests.addQuestEventListeners();
     achievements.addAchievementEventListeners();
     guide.addGuideEventListeners();
+    guide.addGuideAlignmentFormEventListener();
     quests.toggleBtnBackgroundStyle();
     quests.validateAllBtnStyle();
-    quests.updateQuestsAchievementTitle();
+    nav.addKeysEventListeners();
+
+    eventsInitialized = true;
 };
 
 // Fonction pour purger les événements
 const purgeEvents = () => {
-
+    if (!eventsInitialized) return;
     nav.removeNavEventListeners();
     quests.removeQuestEventListeners();
     achievements.removeAchievementEventListeners();
     guide.removeGuideEventListeners();
-};
 
-// Écouteur pour `turbo:before-render`
-document.addEventListener("turbo:before-render", (event) => {
-    nav.updateTopNavTitle(event);
-    purgeEvents();
-});
+    eventsInitialized = false;
+};
 
 // Écouteur pour `turbo:load`
 document.addEventListener("turbo:load", () => {
+    console.log("turbo:load");
     initializeEvents();
 });
 
-// Écouteur pour `turbo:frame-render`
-document.addEventListener("turbo:frame-render", () => {
-    purgeEvents();
-    initializeEvents();
+// Écouteur pour `turbo:before-render`
+document.addEventListener("turbo:before-render", (event) => {
+    console.log("turbo:before-render");
+    nav.updateTopNavTitle(event);
 });
+
+document.addEventListener("turbo:frame-render", () => {
+    // quests.updateQuestsAchievementTitle();
+    // quests.toggleBtnBackgroundStyle();
+    // quests.validateAllBtnStyle();
+});
+
+document.addEventListener("turbo:frame-load", (event) => {
+    console.log("frame-load");
+    quests.updateQuestsAchievementTitle();
+    quests.toggleBtnBackgroundStyle();
+    quests.validateAllBtnStyle();
+})
+
+document.addEventListener("turbo:before-visit", () => {
+    console.log("before-visit");
+})
+
+document.addEventListener("turbo:visit", () => {
+    console.log("visit");
+})
+
+document.addEventListener("turbo:submit-start", (e) => {
+    console.log("submit-start");
+})
+
+document.addEventListener("turbo:submit-end", (e) => {
+    console.log("submit-end");
+    setTimeout(() => {
+        quests.toggleBtnBackgroundStyle();
+        quests.validateAllBtnStyle();
+    }, 10);
+})
+

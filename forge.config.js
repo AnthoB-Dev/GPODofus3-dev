c92bbc249f893df3fa47b248fa5beb4a98a21b32
Anthony <bonis.anthony@gmail.com>
2024-11-30 17:59:19 +0100

    Add squirrel config

--- forge.config.js
+++ forge.config.js
@@ -1,37 +1,63 @@
-const { FusesPlugin } = require('@electron-forge/plugin-fuses');
-const { FuseV1Options, FuseVersion } = require('@electron/fuses');
+const path = require("path");
+const { FusesPlugin } = require("@electron-forge/plugin-fuses");
+const { FuseV1Options, FuseVersion } = require("@electron/fuses");
 
 module.exports = {
   packagerConfig: {
-    name: 'GPODofus3',
+    name: "GPODofus3",
     asar: false,
-    icon: 'app',
-    executableName: 'GPOD3'
+    icon: path.join(
+      __dirname,
+      "static",
+      "medias",
+      "icons",
+      "favicons",
+      "icon.ico"
+    ),
+    executableName: "GPOD3",
   },
   rebuildConfig: {},
   makers: [
     {
-      name: '@electron-forge/maker-squirrel',
+      name: "@electron-forge/maker-squirrel",
       config: {
+        name: "GPODofus3",
         authors: "AnthoB-Dev",
         description: "Guide de progression optimisée pour Dofus 3",
+        setupIcon: path.join(
+          __dirname,
+          "static",
+          "medias",
+          "icons",
+          "favicons",
+          "icon.ico"
+        ),
+        noMsi: true,
+        iconUrl: path.join(
+          __dirname,
+          "static",
+          "medias",
+          "icons",
+          "favicons",
+          "icon.ico"
+        ),
+        exe: "GPODofus3.exe",
       },
     },
     {
-      name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
+      name: "@electron-forge/maker-zip",
+      platforms: ["darwin"],
     },
     {
-      name: '@electron-forge/maker-deb',
+      name: "@electron-forge/maker-deb",
       config: {},
     },
     {
-      name: '@electron-forge/maker-rpm',
+      name: "@electron-forge/maker-rpm",
       config: {},
     },
   ],
   plugins: [
-    
     // Fuses sont utilisés pour activer/désactiver diverses fonctionnalités d'Electron
     new FusesPlugin({
       version: FuseVersion.V1,

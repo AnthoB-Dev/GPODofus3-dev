5f6a09543979e67bb16d3e44f150e31bb3a79b5b
Anthony <bonis.anthony@gmail.com>
2024-12-01 16:08:30 +0100

    Add documentations

--- static/js/quests.js
+++ static/js/quests.js
@@ -1,6 +1,11 @@
-import { handleAchievementButtonClick, getAchievements } from "./achievements.js";
+import { getAchievements } from "./achievements.js";
 
 
+/**
+ * - Récupère les quêtes visibles.
+ * - Clique sur leurs liens.
+ * @returns {Promise<void>}
+ */
 const openLinksOfQuests = async () => {
     const links = getLinksOfQuests();
     if (!links) return;
@@ -17,6 +22,11 @@ const openLinksOfQuests = async () => {
     }
 };
 
+/**
+ * - Récupère les liens des quêtes visibles.
+ * - Retourne un tableau contenant les liens.
+ * @returns {Array}
+ */
 const getLinksOfQuests = () => {
     const jsQuest = document.querySelectorAll(".js-quest");
     if (jsQuest.length === 0) return;
@@ -30,6 +40,12 @@ const getLinksOfQuests = () => {
     return links;
 };
 
+/**
+ * - Gère le clic sur le bouton "Ouvrir tout".
+ * - Utilise la fonction openLinksOfQuests( ) pour ouvrir les liens des quêtes.
+ * - Set un timeout de 3s sur le bouton "Ouvrir tout" pour éviter d'avoir 10*nb_clic d'onglets qui s'ouvrent.
+ * @returns {Promise<void>}
+ */
 const handleOpenAllBtnClick = async () => {
     const openAll = document.querySelector("#openAll");
     if (openAll && openAll.disabled) {
@@ -37,9 +53,16 @@ const handleOpenAllBtnClick = async () => {
     }
     openAll.disabled = true; 
     await openLinksOfQuests();
-    openAll.disabled = false;
+    setTimeout(() => {
+        openAll.disabled = false;
+    }, 3000);
 };
 
+/**
+ * - Gère le clic sur le bouton "Valider tout".
+ * - Utilise la fonction clickValidateAll( ) pour valider/dévalider toutes les quêtes.
+ * @returns {Promise<void>}
+ */
 const handleValidateAllBtnClick = async () => {
     const validateAll = document.querySelector("#validateAll");
     if (validateAll && validateAll.disabled) {
@@ -48,6 +71,9 @@ const handleValidateAllBtnClick = async () => {
     await clickValidateAll();
 };
 
+/**
+ * - Ajoute les écouteurs d'événements sur les boutons "Ouvrir tout" et "Valider tout".
+ */
 export const addQuestEventListeners = () => {
     const openAll = document.querySelector("#openAll");
     if (openAll) {
@@ -64,6 +90,9 @@ export const addQuestEventListeners = () => {
     updateQuestsAchievementTitle();
 };
 
+/**
+ * - Supprime les écouteurs d'événements sur les boutons "Ouvrir tout" et "Valider tout".
+ */
 export const removeQuestEventListeners = () => {
     const openAll = document.querySelector("#openAll");
     if (openAll) {
@@ -76,6 +105,10 @@ export const removeQuestEventListeners = () => {
     }
 };
 
+/**
+ * - Change dynamiquement le contenu du bouton "Valider tout" en fonction de la validation des quêtes.
+ * @returns {Promise<void>}
+ */
 export const validateAllBtnStyle = async () => {
     const validateAll = document.querySelector("#validateAll");
     if (!validateAll) return;
@@ -101,6 +134,10 @@ export const validateAllBtnStyle = async () => {
     }
 };
 
+/**
+ * - Changement dynamiquement le style des boutons de validation des quêtes .qButton lors du survol du bouton "Valider tout".
+ * @returns {Promise<void>}
+ */
 export const toggleBtnBackgroundStyle = async () => {
     const validateAll = document.querySelector("#validateAll");
     let buttons = getButtons();
@@ -123,6 +160,11 @@ export const toggleBtnBackgroundStyle = async () => {
     });
 };
 
+/**
+ * - Récupère les boutons de validation des quêtes.
+ * - Retourne un tableau contenant les boutons.
+ * @returns {Array}
+ */
 const getButtons = () => {
     const checkBtns = document.querySelectorAll(".js-quest .check");
     const uncheckBtns = document.querySelectorAll(".js-quest .uncheck");
@@ -136,6 +178,11 @@ const getButtons = () => {
     return tglCompletionBtns;
 };
 
+/**
+ * - Clique sur le bouton de validation de la quête qui est passé en paramètre.
+ * @param {qButton} button 
+ * @returns {Promise<void>}
+ */
 const toggleBtn = async (button) => {
     return new Promise((resolve) => {
         if (document.body.contains(button)) {
@@ -147,6 +194,12 @@ const toggleBtn = async (button) => {
     });
 }
 
+/**
+ * - Clique sur tous les boutons de validation des quêtes.
+ * - Désactive le bouton "Valider tout" pendant la validation.
+ * - Utilise la fonction toggleBtn( ) pour cliquer sur chaque bouton.
+ * @returns {Promise<void>}
+ */
 const clickValidateAll = async () => {
     const validateAll = document.querySelector("#validateAll");
     if (validateAll) {
@@ -154,7 +207,6 @@ const clickValidateAll = async () => {
     }
 
     let tglCompletionBtns = getButtons();
-    const achievements = Array.from(await getAchievements());
 
     if (!Array.isArray(tglCompletionBtns)) {
         tglCompletionBtns = Array.from(tglCompletionBtns);
@@ -164,13 +216,15 @@ const clickValidateAll = async () => {
         await toggleBtn(button);
     }
 
-    handleAchievementButtonClick(tglCompletionBtns[0], achievements);
-
     if (validateAll) {
         validateAll.disabled = false; 
     }
 };
 
+/**
+ * - Met à jour le titre du succès dans la section Quêtes #quest_frame_achievement_title
+ * @returns {Promise<void>}
+ */
 export const updateQuestsAchievementTitle = async () => {
     const questsAchievementId = document.querySelector(".js-quest")?.dataset.achievementId;
     if (questsAchievementId === undefined) return;
